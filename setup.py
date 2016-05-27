d23beda131bada0749995dbec8d427ee35b17aef
sapetnioc <sapetnioc@users.noreply.github.com>
2016-05-27 13:44:25 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
 requires = [
     'pyramid',
     'pyramid_jinja2',
@@ -28,7 +22,7 @@ tests_require = [
 setup(name='cati_manager',
       version='0.0',
       description='cati_manager',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='',
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
