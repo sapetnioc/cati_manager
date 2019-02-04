f301105b8caace5cfbb0edba817bf12c1cf23f5c
sapetnioc <sapetnioc@users.noreply.github.com>
2019-02-04 11:11:26 +0100

    Improved and simplified definition of fields that compose the configuration of the server

--- cati_portal/http/settings.py
+++ cati_portal/http/settings.py
@@ -5,28 +5,47 @@ import os.path as osp
 from flask import (Blueprint, render_template, url_for, redirect, current_app)
 from flask_wtf import FlaskForm
 from flask_login import login_required
-from wtforms import StringField, SubmitField
-
-from cati_portal.db import get_db
+from wtforms import StringField, PasswordField, SubmitField
 
 bp = Blueprint('settings', __name__, url_prefix='/settings')
 
 
 class SettingsForm(FlaskForm):
-    smtp_server = StringField('smtp_server')
+    '''
+    This form defines the fields that compose the cati_portal server
+    configuration. These fields can be edited in the /settings page and
+    are stored in the flask server configuration. This configuration is
+    stored on disk in a JSON file that is read at server startup.
+    '''
+    smtp_server = StringField('SMTP server')
+    smtp_login = StringField('SMTP login')
+    smtp_password = PasswordField('SMTP password')
+    
     submit = SubmitField('Save settings')
 
 @bp.route('', methods=('GET', 'POST'))
 @login_required
 def settings():
-    with get_db() as db:
-        form = SettingsForm(smtp_server = current_app.config.get('SMTP_SERVER', ''))
-        if form.validate_on_submit():
-            current_app.config['SMTP_SERVER'] = form.smtp_server.data
-            config_file = osp.join(current_app.instance_path, 'config.json')
-            if osp.exists(config_file):
-                config = json.load(open(config_file))
-                config['SMTP_SERVER'] = current_app.config['SMTP_SERVER']
-                json.dump(config, open(config_file, 'w'), indent=4)
-            return redirect(url_for('home.index'))
-        return render_template('form_page.html', title='Server settings', form=form)
+    form = SettingsForm()
+    config = None
+    if form.validate_on_submit():
+        config_file = osp.join(current_app.instance_path, 'config.json')
+        if osp.exists(config_file):
+            config = json.load(open(config_file))
+        for field in form:
+            if field.type == 'SubmitField' or field.short_name == 'csrf_token':
+                continue
+            if field.type == 'PasswordField' and not field.data:
+                continue
+            current_app.config[field.short_name.upper()] = field.data
+            if config is not None:
+                config[field.short_name.upper()] = field.data
+        if config is not None:
+            json.dump(config, open(config_file, 'w'), indent=4)
+        # return redirect(url_for('home.index'))
+    else:
+        for field in form:
+            value = current_app.config.get(field.short_name.upper())
+            if value is not None:
+                field.data = value
+    return render_template('form_page.html', title='Server settings', form=form)
