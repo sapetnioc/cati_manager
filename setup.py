ba18f71b80326421dd82833bb19028c51ad149dc
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-02-05 07:37:21 +0000

    Setup: add script, add information to __init__

--- setup.py
+++ setup.py
@@ -1,48 +1,55 @@
-import os
-
 from setuptools import setup, find_packages
+from cati_portal import __version__
+from cati_portal import __author__
+from cati_portal import __email__
+from cati_portal import __license__
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
-requires = [
-    'flask >= 1.0',
-    'flask-login',
-    'flask-wtf',
-    'psycopg2-binary >= 2.7',
-    'click >= 5.0',
-    'gunicorn',
-]
+def readme():
+    with open('README.txt') as f:
+        return f.read()
 
-tests_require = [
-    #'WebTest >= 1.3.1',  # py3 compat
-    #'pytest',
-    #'pytest-cov',
-]
+
+def changes():
+    with open('CHANGES.txt') as f:
+        return f.read()
 
 setup(
     name='cati_portal',
-    version='0.0',
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
     description='CATI Portal',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=readme() + '\n\n' + changes(),
+    license=__license__,
+    url='https://github.com/sapetnioc/cati_portal',
     classifiers=[
         'Programming Language :: Python',
-        'Framework :: Pyramid',
+        "Programming Language :: Python :: 3",
+        'Framework :: Flask',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
-    author='',
-    author_email='',
-    url='',
     keywords='web flask',
     packages=find_packages(),
+    scripts=[
+        'cati_portal_ctl',
+    ],
     include_package_data=True,
     zip_safe=False,
+    install_requires=[
+        'flask >= 1.0',
+        'flask-login',
+        'flask-wtf',
+        'psycopg2-binary >= 2.7',
+        'click >= 5.0',
+        'gunicorn',
+    ],
     extras_require={
-        'testing': tests_require,
+        'testing': [
+            #'WebTest >= 1.3.1',  # py3 compat
+            #'pytest',
+            #'pytest-cov',
+        ],
     },
-    install_requires=requires,
 )
